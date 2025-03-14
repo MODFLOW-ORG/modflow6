9d65cb094c1b73fa8a35c218bf2eaffc26af1b66
Joshua Larsen <jlarsen@usgs.gov>
2025-03-14 16:09:18 -0700

    Update test_gwf_newton_under_relaxation.py
    update again

--- autotest/test_gwf_newton_under_relaxation.py
+++ autotest/test_gwf_newton_under_relaxation.py
@@ -106,7 +106,9 @@ def build_models(idx, test):
 def check_output(idx, test):
     mf6sim = flopy.mf6.MFSimulation.load(sim_ws=test.workspace)
     if idx == 1:
-        mfsplit = Mf6Spliiter.load_node_mapping(pl.Path(f"{test.workspace}/mapping.h5"))
+        mfsplit = flopy.mf6.utils.Mf6Splitter.load_node_mapping(
+            pl.Path(f"{test.workspace}/mapping.h5")
+        )
         head_dict = {}
         for modelname in mf6sim.model_names:
             mnum = int(modelname.split("_")[-1])
