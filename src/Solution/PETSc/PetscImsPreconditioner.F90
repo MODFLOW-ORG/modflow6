043ffd0accf571d74d8907c09de00ff246a66616
mjr-deltares <Martijn.Russcher@deltares.nl>
2025-10-30 08:00:20 +0100

    - level=0 and drop_tol > 0 now activates (M)ILUT for PETSc as well

--- src/Solution/PETSc/PetscImsPreconditioner.F90
+++ src/Solution/PETSc/PetscImsPreconditioner.F90
@@ -71,7 +71,10 @@ contains
     this%linear_settings => lin_settings
 
     this%ipc = 1
-    if (this%linear_settings%level > 0) this%ipc = this%ipc + 2
+    if (this%linear_settings%level > 0 .or. &
+        this%linear_settings%droptol > 0.0_DP) then
+      this%ipc = this%ipc + 2
+    end if
     if (this%linear_settings%relax > 0.0_DP) this%ipc = this%ipc + 1
     this%ims_pc_type = IMS_PC_TYPE(this%ipc)
 
