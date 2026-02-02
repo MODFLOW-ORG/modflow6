8d0a45fce3436daa4ce8e32287e62177efdb56f0
mjr-deltares <martijn.russcher@deltares.nl>
2026-02-02 15:56:53 +0100

    - add steady state test

--- autotest/test_gwf_uzr_steady.py
+++ autotest/test_gwf_uzr_steady.py
@@ -12,17 +12,24 @@ from framework import TestFramework
 from gwf_test_utils import PLOT_UZR_TESTS, get_uzr_soil_data
 from modflow_devtools.misc import is_in_ci
 
-cases = ["uzr-steady", "uzr-trans"]
-steady = [True, False]
+cases = ["uzr-trs-pic", "uzr-trs-nwt", "uzr-std-pic", "uzr-std-nwt"]
+steadystate = [False, False, True, True]
+newton = [False, True, False, True]
 
 
 def build_models(idx, test):
     column_height = 42.0
     nlay, nrow, ncol = 42, 1, 1
-    nper = 1
-    perlen = [360.0]  # s
-    nstp = [1]
-    tsmult = [1.0]
+    nper = 10
+    perlen = nper * [360.0]  # s
+    tsmult = nper * [1.0]
+    nstp = nper * [1]
+    if steadystate[idx]:
+        nper = 1
+        perlen = [sum(perlen)]
+        tsmult = [1.0]
+        nstp = [1]
+
     delr = 10.0  # cm
     delc = 10.0
     delz = column_height / nlay
@@ -40,7 +47,7 @@ def build_models(idx, test):
     hstart = [botm[i] + 0.5 * delz + hp_lower for i in range(nlay)]
     strt[:, 0, 0] = hstart[:]
 
-    nouter, ninner = 100, 300
+    nouter, ninner = 200, 300
     hclose, rclose, relax = 1e-6, 1e-6, 0.97
 
     tdis_rc = []
@@ -59,13 +66,16 @@ def build_models(idx, test):
 
     # create gwf model
     gwfname = "gwf_" + name
-    gwf = flopy.mf6.MFModel(
+    nwtopts = ""
+    if newton[idx]:
+        nwtopts = "newton"
+    gwf = flopy.mf6.ModflowGwf(
         sim,
-        model_type="gwf6",
         modelname=gwfname,
         model_nam_file=f"{gwfname}.nam",
+        save_flows=True,
+        newtonoptions=nwtopts,
     )
-    gwf.name_file.save_flows = True
 
     # create iterative model solution and register the gwf model with it
     imsgwf = flopy.mf6.ModflowIms(
@@ -73,16 +83,22 @@ def build_models(idx, test):
         print_option="SUMMARY",
         outer_dvclose=hclose,
         outer_maximum=nouter,
-        under_relaxation="NONE",
+        under_relaxation="None",
         inner_maximum=ninner,
         inner_dvclose=hclose,
         rcloserecord=rclose,
-        linear_acceleration="CG",
+        linear_acceleration="BICGSTAB",
         scaling_method="NONE",
         reordering_method="NONE",
         relaxation_factor=relax,
         filename=f"{gwfname}.ims",
     )
+    if not newton[idx]:
+        imsgwf.under_relaxation = ("DBD",)
+        imsgwf.under_relaxation_theta = (0.9,)
+        imsgwf.under_relaxation_kappa = (0.0001,)
+        imsgwf.under_relaxation_gamma = (0.0,)
+
     sim.register_ims_package(imsgwf, [gwf.name])
 
     dis = flopy.mf6.ModflowGwfdis(
@@ -111,8 +127,8 @@ def build_models(idx, test):
         ss=0.0001,
         sy=0.3,
         iconvert=0,
-        transient={0: not steady[idx]},
-        steady_state={0: steady[idx]},
+        transient=not steadystate[idx],
+        steady_state=steadystate[idx],
     )
 
     # unsaturated zone Richards flow
@@ -120,6 +136,7 @@ def build_models(idx, test):
     uzr = flopy.mf6.ModflowGwfuzr(
         gwf,
         iunsat=1,
+        phead_filerecord=f"{gwfname}.phd",
         storage_scheme="chord-slope",
         kr_averaging="harmonic",
         soil_model="Haverkamp",
@@ -148,15 +165,23 @@ def build_models(idx, test):
         budget_filerecord=f"{gwfname}.cbc",
         head_filerecord=f"{gwfname}.hds",
         headprintrecord=[("COLUMNS", 10, "WIDTH", 15, "DIGITS", 6, "GENERAL")],
-        saverecord=[("HEAD", "LAST"), ("BUDGET", "LAST")],
+        saverecord=[("HEAD", "ALL"), ("BUDGET", "ALL")],
         printrecord=[("HEAD", "LAST"), ("BUDGET", "LAST")],
     )
 
     return sim, None
 
 
+saved_profiles = {}
+
+
 def check_output(idx, test):
     model_name = "gwf_" + test.name
+    fpth = os.path.join(test.workspace, f"{model_name}.phd")
+    pfile = flopy.utils.HeadFile(fpth)
+    times = pfile.get_times()
+
+    saved_profiles[idx] = pfile.get_data(totim=times[-1])[:, 0, 0]
 
     fpth = os.path.join(test.workspace, f"{model_name}.dis.grb")
     grb = flopy.mf6.utils.MfGrdFile(fpth)
@@ -164,19 +189,33 @@ def check_output(idx, test):
     nlay = mg.nlay
     dz = mg.delz.flatten()
     botm = mg.botm.flatten()
-
-    fpth = os.path.join(test.workspace, f"{model_name}.hds")
-    hds = flopy.utils.HeadFile(fpth)
-    heads = hds.get_data(idx=-1).flatten()
-
-    pheads = [heads[ilay] - botm[ilay] - 0.5 * dz[ilay] for ilay in range(nlay)]
     depth = [-botm[ilay] - 0.5 * dz[ilay] for ilay in range(nlay)]
 
+    figpth = os.path.join(test.workspace, f"pressure_head-{cases[idx]}.png")
     if PLOT_UZR_TESTS and not is_in_ci():
-        plt.plot(depth, pheads)
+        plt.figure(figsize=(12, 6))
+
+        for i, t in enumerate(times):
+            pheads = pfile.get_data(totim=t)[:, 0, 0]
+            label = f"{cases[idx]}: @t={times[-1]}"
+            plt.plot(depth, pheads, label=label)
+
+        # if last, plot all final profiles of previous in same graph
+        if idx == len(cases) - 1:
+            mrk = ["o", "x", "v", ">", "<", "^"]
+            for iprev in range(len(cases) - 1):
+                plt.plot(
+                    depth,
+                    saved_profiles[iprev],
+                    marker=mrk[iprev],
+                    linestyle="None",
+                    label=f"{cases[iprev]}: @t={times[-1]}",
+                )
+
         plt.xlim(0.0, 40.0)
         plt.ylim(-70.0, 0.0)
-        plt.savefig(f"pressure_head-{cases[idx]}.png")
+        plt.legend()
+        plt.savefig(figpth)
 
 
 @pytest.mark.parametrize("idx, name", enumerate(cases))
@@ -187,6 +226,5 @@ def test_mf6model(idx, name, function_tmpdir, targets):
         build=lambda t: build_models(idx, t),
         check=lambda t: check_output(idx, t),
         targets=targets,
-        xfail=steady[idx],
     )
     test.run()
