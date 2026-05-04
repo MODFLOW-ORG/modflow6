d970675243b399a3bc780ef462faaf40e5cce76a
Sunny Titus <sunny.titus@deltares.nl>
2026-05-05 00:05:39 +0200

    Change make install command

--- .github/common/compile_petsc.sh
+++ .github/common/compile_petsc.sh
@@ -1,4 +1,5 @@
 cd "$GITHUB_WORKSPACE/petsc"
+PETSC_PREFIX=$(cygpath -u "$GITHUB_WORKSPACE/petsc")
 make all
 make check
-make install
+make PETSC_DIR=$PETSC_PREFIX PETSC_ARCH=arch-mswin-c-opt install
