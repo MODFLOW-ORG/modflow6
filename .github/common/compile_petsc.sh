d51526080b6c853b3df77e693d2aa55443020e34
Sunny Titus <sunny.titus@deltares.nl>
2026-05-05 10:57:11 +0200

    Remove install folder before installing petsc

--- .github/common/compile_petsc.sh
+++ .github/common/compile_petsc.sh
@@ -2,4 +2,5 @@ cd "$GITHUB_WORKSPACE/petsc"
 PETSC_PREFIX=$(cygpath -u "$GITHUB_WORKSPACE/petsc")
 make all
 make check
+rm -rf "$PETSC_PREFIX/install"
 make PETSC_DIR=$PETSC_PREFIX PETSC_ARCH=arch-mswin-c-opt install
