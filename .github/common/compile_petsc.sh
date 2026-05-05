0d7b975fd2a961b68d1d005c52b9c87ef5358535
Sunny Titus <sunny.titus@deltares.nl>
2026-05-05 14:24:47 +0200

    Test if make install can be simplified. Fix the petsc lib location in the release.yml

--- .github/common/compile_petsc.sh
+++ .github/common/compile_petsc.sh
@@ -2,5 +2,7 @@ cd "$GITHUB_WORKSPACE/petsc"
 PETSC_PREFIX=$(cygpath -u "$GITHUB_WORKSPACE/petsc")
 make all
 make check
+
+# We need to make sure this folder doesn't exist otherwise the install target does nothing.
 rm -rf "$PETSC_PREFIX/install"
-make PETSC_DIR=$PETSC_PREFIX PETSC_ARCH=arch-mswin-c-opt install
+make install
