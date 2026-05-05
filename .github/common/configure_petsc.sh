d4c91c03de9a0d81184d17afc1580b2bc6a03894
Sunny Titus <sunny.titus@deltares.nl>
2026-05-05 11:43:58 +0200

    Add mpi flags

--- .github/common/configure_petsc.sh
+++ .github/common/configure_petsc.sh
@@ -17,6 +17,7 @@ env -i \
     --with-cc='cl' \
     --with-fc='ifort' \
     --with-cxx='cl' \
+    FPPFLAGS='-I/cygdrive/c/PROGRA~2/Intel/oneAPI/mpi/latest/include/mpi' \
     --with-blaslapack-lib='-L/cygdrive/c/PROGRA~2/Intel/oneAPI/mkl/latest/lib mkl_intel_lp64_dll.lib mkl_sequential_dll.lib mkl_core_dll.lib' \
     --with-mpi-include='/cygdrive/c/PROGRA~2/Intel/oneAPI/mpi/latest/include' \
     --with-mpi-lib='/cygdrive/c/PROGRA~2/Intel/oneAPI/mpi/latest/lib/impi.lib' \
