98023678ef061f244069fcde1f3e2fdeb9b8f999
wpbonelli <wpbonelli@gmail.com>
2026-08-18 12:18:51 -0400

    improve docstrings and comments

--- src/Utilities/GeomUtil.f90
+++ src/Utilities/GeomUtil.f90
@@ -21,14 +21,9 @@ contains
   !> @brief Check if a point is within a polygon.
   !!
   !! Vertices and edge points are considered in the polygon. By
-  !! default, a point lying exactly on an edge (in floating point
-  !! terms) is required for the point to be considered on that edge.
-  !! An optional tolerance may be given to instead accept a point
-  !! within tol of an edge, in the sense that the (twice-signed-area)
-  !! cross product used for the edge test is within tol of zero; this
-  !! is a caller's responsibility to scale appropriately (e.g. to the
-  !! size of the polygon), as this routine has no basis on which to
-  !! choose a tolerance itself.
+  !! default, a point must lie exactly on an edge to be considered
+  !! within the polygon. A tolerance may be specified to instead
+  !! accept coordinates within tol of an edge. 
   !!
   !! Adapted from https://stackoverflow.com/a/63436180/6514033,
   !<
@@ -37,7 +32,7 @@ contains
     real(DP), intent(in) :: x !< x point coordinate
     real(DP), intent(in) :: y !< y point coordinate
     real(DP), allocatable, intent(in) :: poly(:, :) !< polygon vertices (column-major indexing)
-    real(DP), intent(in), optional :: tol !< tolerance for the on-edge cross product test (default 0)
+    real(DP), intent(in), optional :: tol !< tolerance (default 0)
     ! local
     integer(I4B) :: i, ii, num_verts
     real(DP) :: xa, xb, ya, yb, c
