0890674b5692c199cf2dd738cb4b8110e8edf685
mjreno <renomik@gmail.com>
2026-09-26 13:57:36 -0400

    suppress write_lstfile for advanced

--- src/Model/GroundWaterFlow/gwf-sfr.f90
+++ src/Model/GroundWaterFlow/gwf-sfr.f90
@@ -1854,7 +1854,11 @@ contains
         ! -- echo row to period data table
         if (this%iprpak /= 0) then
           call this%inputtab%add_term(n)
-          call this%inputtab%add_term(trim(setting))
+          if (trim(setting) == 'PERIOD_AUXILIARY') then
+            call this%inputtab%add_term('AUXILIARY')
+          else
+            call this%inputtab%add_term(trim(setting))
+          end if
           select case (trim(setting))
           case ('STATUS')
             call this%inputtab%add_term(trim(str))
